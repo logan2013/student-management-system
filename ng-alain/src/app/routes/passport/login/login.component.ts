cea8e10c74cb01514608b75336962b95209dce8f
YDeity <meetliuyuan@163.com>
2018-05-31 09:52:14 +0800

    修改登陆页
    修改登陆页

--- ng-alain/src/app/routes/passport/login/login.component.ts
+++ ng-alain/src/app/routes/passport/login/login.component.ts
@@ -5,7 +5,6 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NzMessageService, NzModalService } from 'ng-zorro-antd';
 import {
   SocialService,
-  SocialOpenType,
   TokenService,
   DA_SERVICE_TOKEN,
 } from '@delon/auth';
@@ -83,7 +82,6 @@ export class UserLoginComponent implements OnDestroy {
   }
 
   // endregion
-
   submit() {
     this.error = '';
     if (this.type === 0) {
@@ -128,49 +126,6 @@ export class UserLoginComponent implements OnDestroy {
       // 否则直接跳转
       this.router.navigate(['/']);
     }, 1000);
-  }
-
-  // region: social
-
-  open(type: string, openType: SocialOpenType = 'href') {
-    let url = ``;
-    let callback = ``;
-    if (environment.production)
-      callback = 'https://cipchk.github.io/ng-alain/callback/' + type;
-    else callback = 'http://localhost:4200/callback/' + type;
-    switch (type) {
-      case 'auth0':
-        url = `//cipchk.auth0.com/login?client=8gcNydIDzGBYxzqV0Vm1CX_RXH-wsWo5&redirect_uri=${decodeURIComponent(
-          callback,
-        )}`;
-        break;
-      case 'github':
-        url = `//github.com/login/oauth/authorize?client_id=9d6baae4b04a23fcafa2&response_type=code&redirect_uri=${decodeURIComponent(
-          callback,
-        )}`;
-        break;
-      case 'weibo':
-        url = `https://api.weibo.com/oauth2/authorize?client_id=1239507802&response_type=code&redirect_uri=${decodeURIComponent(
-          callback,
-        )}`;
-        break;
-    }
-    if (openType === 'window') {
-      this.socialService
-        .login(url, '/', {
-          type: 'window',
-        })
-        .subscribe(res => {
-          if (res) {
-            this.settingsService.setUser(res);
-            this.router.navigateByUrl('/');
-          }
-        });
-    } else {
-      this.socialService.login(url, '/', {
-        type: 'href',
-      });
-    }
   }
 
   // endregion
