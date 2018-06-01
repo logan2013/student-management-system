2a5986456d65b70a136b520ebf13e443dd7fbe22
YDeity <meetliuyuan@163.com>
2018-06-01 23:19:15 +0800

    <移除> Swagger2 的使用 ， 存在 bug ， 日后解决 ！！！！！
    <移除> Swagger2 的使用 ， 存在 bug ， 日后解决 ！！！！！

--- spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/config/Swagger2Configuration.java
+++ spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/config/Swagger2Configuration.java
@@ -2,6 +2,7 @@ package cn.imust.ys.springbootshiro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,11 +11,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+/*@Configuration
 @EnableSwagger2
+@EnableWebMvc*/
 public class Swagger2Configuration {
 
-	@Bean
+//	@Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
