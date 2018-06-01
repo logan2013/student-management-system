467f6a14b309b9e01f21c028fb71bd4110f8b539
YDeity <meetliuyuan@163.com>
2018-06-01 23:15:53 +0800

    <修改> 增加允许 token 的请求头
    <修改> 增加允许 token 的请求头

--- spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/filter/CorsFilter.java
+++ spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/filter/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, x-auth-token, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, x-auth-token,token, X-Requested-With, Content-Type, Accept");
         System.out.println("*********************************过滤器被使用**************************");
         HttpServletRequest request = (HttpServletRequest)req;
         if (("OPTIONS").equalsIgnoreCase(request.getMethod())){
