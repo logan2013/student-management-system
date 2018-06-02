7627066c0dc490f8a18168a3c729885a2d57a754
YDeity <meetliuyuan@163.com>
2018-06-02 15:30:19 +0800

    <增加> 个人中心设置路由

--- ng-alain/src/app/layout/default/header/components/user.component.ts
+++ ng-alain/src/app/layout/default/header/components/user.component.ts
@@ -12,8 +12,7 @@ import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
       {{settings.user.name}}
     </div>
     <div nz-menu class="width-sm">
-      <div nz-menu-item [nzDisabled]="true"><i class="anticon anticon-user mr-sm"></i>个人中心</div>
-      <div nz-menu-item [nzDisabled]="true"><i class="anticon anticon-setting mr-sm"></i>设置</div>
+      <div nz-menu-item (click)="account()"><i class="anticon anticon-user mr-sm"></i>个人中心</div>
       <li nz-menu-divider></li>
       <div nz-menu-item (click)="logout()"><i class="anticon anticon-setting mr-sm"></i>退出登录</div>
     </div>
@@ -44,6 +43,10 @@ export class HeaderUserComponent implements OnInit {
       email: '1334112864@qq.com',
     };
     this.tokenService.set(token);
+  }
+
+  account(){
+    this.router.navigate(['/extras/settings']);
   }
 
   logout() {
