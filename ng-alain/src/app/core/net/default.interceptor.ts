8420af117a5677ec2d394026bb7147abfb77bbbc
YDeity <meetliuyuan@163.com>
2018-06-13 14:09:38 +0800

    <修改> 前台过滤器信息处理

--- ng-alain/src/app/core/net/default.interceptor.ts
+++ ng-alain/src/app/core/net/default.interceptor.ts
@@ -22,7 +22,8 @@ import { environment } from '@env/environment';
  */
 @Injectable()
 export class DefaultInterceptor implements HttpInterceptor {
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector) {
+  }
 
   get msg(): NzMessageService {
     return this.injector.get(NzMessageService);
@@ -45,20 +46,21 @@ export class DefaultInterceptor implements HttpInterceptor {
         //  错误内容：{ status: 1, msg: '非法参数' }
         //  正确内容：{ status: 0, response: {  } }
         // 则以下代码片断可直接适用
-        // if (event instanceof HttpResponse) {
-        //     const body: any = event.body;
-        //     if (body && body.status !== 0) {
-        //         this.msg.error(body.msg);
-        //         // 继续抛出错误中断后续所有 Pipe、subscribe 操作，因此：
-        //         // this.http.get('/').subscribe() 并不会触发
-        //         return throwError({});
-        //     } else {
-        //         // 重新修改 `body` 内容为 `response` 内容，对于绝大多数场景已经无须再关心业务状态码
-        //         return of(new HttpResponse(Object.assign(event, { body: body.response })));
-        //         // 或者依然保持完整的格式
-        //         return of(event);
-        //     }
-        // }
+        if (event instanceof HttpResponse) {
+          const body: any = event.body;
+          if (body && body.code === 401) {
+            this.msg.error(body.msg);
+            // 继续抛出错误中断后续所有 Pipe、subscribe 操作，因此：
+            // this.http.get('/').subscribe() 并不会触发
+            this.goTo('/passport/login');
+            return throwError({});
+          } else {
+            // 重新修改 `body` 内容为 `response` 内容，对于绝大多数场景已经无须再关心业务状态码
+            // return of(new HttpResponse(Object.assign(event, { body: body.response })));
+            // 或者依然保持完整的格式
+            return of(event);
+          }
+        }
         break;
       case 401: // 未登录状态码
         this.goTo('/passport/login');
@@ -84,13 +86,11 @@ export class DefaultInterceptor implements HttpInterceptor {
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler,
-  ): Observable<
-    | HttpSentEvent
+  ): Observable<| HttpSentEvent
     | HttpHeaderResponse
     | HttpProgressEvent
     | HttpResponse<any>
-    | HttpUserEvent<any>
-  > {
+    | HttpUserEvent<any>> {
     // 统一加上服务端前缀
     let url = req.url;
     if (url.startsWith('assets')) {
