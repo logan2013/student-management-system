69a15791d4be4b7f50a92c97b562435babf70b37
YDeity <meetliuyuan@163.com>
2018-06-19 19:21:02 +0800

    <添加> 学号不为空的查询方法

--- spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/modules/student/repository/StudentRepository.java
+++ spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/modules/student/repository/StudentRepository.java
@@ -4,8 +4,12 @@ import cn.imust.ys.springbootshiro.modules.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>{
 
     Student findBySno(String sno);
+
+    List<Student> findBySnoIsNotNull();
 }
