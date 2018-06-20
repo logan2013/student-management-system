6c9925465d6f204376c66b9f0d4058973374a633
YDeity <meetliuyuan@163.com>
2018-06-21 00:21:36 +0800

    <添加> 通过全名找班级方法

--- spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/modules/system/repository/SysClassRepository.java
+++ spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/modules/system/repository/SysClassRepository.java
@@ -4,6 +4,9 @@ import cn.imust.ys.springbootshiro.modules.system.entity.SysClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SysClassRepository extends JpaRepository<SysClass,Integer> {
+    List<SysClass> findByAllName(String allName);
 }
