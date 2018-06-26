126ed7512426323ddfc2d83462d30ba035ad2fd7
YDeity <meetliuyuan@163.com>
2018-06-26 18:41:26 +0800

    <删除>无用注释

--- spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/handler/ErrorExceptionHandler.java
+++ spring-boot-shiro/src/main/java/cn/imust/ys/springbootshiro/handler/ErrorExceptionHandler.java
@@ -4,7 +4,6 @@ import cn.imust.ys.springbootshiro.utils.ControllerUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
